eb934757fac7e8ead751ee4437af0e6ccd47130c
Tendi F <tendifirmansyah30@gmail.com>
2021-02-14 00:33:59 +0700

    lint

--- fontend/src/pages/Shift.js
+++ fontend/src/pages/Shift.js
@@ -7,7 +7,7 @@ import {
   useRouteMatch,
   useHistory
 } from "react-router-dom"
-import {NotificationManager} from 'react-notifications';
+import { NotificationManager } from 'react-notifications';
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
@@ -17,39 +17,36 @@ import { getUser } from '../app/userReducer'
 import { getAxiosShift, getShift } from '../app/shiftReducer'
 
 export default function Shift() {
-  const {user} = useSelector(getUser);
+  const { user } = useSelector(getUser);
   let { path, url } = useRouteMatch();
   return <>
-    {!user.data.id && <Redirect to="/login" />} 
+    {!user.data.id && <Redirect to="/login" />}
     <div className="header">
       <h4>Shift {user.data ? user.data.id : ""}</h4>
     </div>
     <Switch>
-        <Route exact path={path}>
-         <ul>
-            <li>
-              <Link to={`${url}/form`}>Add Shift</Link>
-            </li>
-            <li>
-              <Link to={`${url}/1`}>Detail </Link>
-            </li>
-          </ul>
-          <ListShift user={user.data}/>
-        </Route>
-        <Route path={`${path}/form`}>
-          <FormShift user={user.data} />
-        </Route>
-        <Route path={`${path}/:id`}>
-          <FormShift type="edit"/>
-        </Route>
+      <Route exact path={path}>
+        <ul>
+          <li>
+            <Link to={`${url}/form`}>Add Shift</Link>
+          </li>
+        </ul>
+        <ListShift user={user.data} />
+      </Route>
+      <Route path={`${path}/form`}>
+        <FormShift user={user.data} />
+      </Route>
+      <Route path={`${path}/:id`}>
+        <FormShift type="edit" />
+      </Route>
     </Switch>
   </>
 }
 
 
-function FormShift(props){
+function FormShift(props) {
   let history = useHistory()
-  let {user, type} = props
+  let { user, type } = props
   let { id } = useParams();
   let [start_date, setStartDate] = useState("");
   let [end_date, setEndDate] = useState("");
@@ -70,7 +67,7 @@ function FormShift(props){
         id
       }
     }).then(res => {
-      if(res.data.data.length){
+      if (res.data.data.length) {
         console.log("update cuy", res.data.data)
         setStartDate(res.data.data[0].start_date)
         setEndDate(res.data.data[0].end_date)
@@ -79,7 +76,7 @@ function FormShift(props){
         history.push("/shift")
         NotificationManager.error("not found", '', 3000);
       }
-      
+
     }).catch(err => {
       NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
     })
@@ -87,111 +84,111 @@ function FormShift(props){
 
   useEffect(() => {
     getUsers()
-    if(type == 'edit'){
+    if (type == 'edit') {
       getShift()
     }
   }, [])
 
   let submitShift = () => {
     let data = {
-      start_date : start_date ? new Date(
-        start_date.substr(0,4),
-        start_date.substr(5,2),
-        start_date.substr(8,2),
-        start_date.substr(11,2),
-        start_date.substr(14,2),
+      start_date: start_date ? new Date(
+        start_date.substr(0, 4),
+        start_date.substr(5, 2),
+        start_date.substr(8, 2),
+        start_date.substr(11, 2),
+        start_date.substr(14, 2),
       ).toISOString() : "",
-      end_date : end_date ? new Date(
-        end_date.substr(0,4),
-        end_date.substr(5,2),
-        end_date.substr(8,2),
-        end_date.substr(11,2),
-        end_date.substr(14,2),
-      ).toISOString(): "",
-      assign_user_id: user_id ? user_id :  user.id
+      end_date: end_date ? new Date(
+        end_date.substr(0, 4),
+        end_date.substr(5, 2),
+        end_date.substr(8, 2),
+        end_date.substr(11, 2),
+        end_date.substr(14, 2),
+      ).toISOString() : "",
+      assign_user_id: user_id ? user_id : user.id
     }
 
-   if(type == "edit") {
-    axios.put('/shift/' + id , data).then(res => {
-      NotificationManager.success('update success', '', 3000);
-    }).catch(err => {
-      NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
-    })
-   } else {
+    if (type == "edit") {
+      axios.put('/shift/' + id, data).then(res => {
+        NotificationManager.success('update success', '', 3000);
+      }).catch(err => {
+        NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
+      })
+    } else {
       axios.post('/shift', data).then(res => {
         NotificationManager.success('add success', '', 3000);
       }).catch(err => {
         NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
       })
-   }
+    }
   }
 
   return <div className="form-shift">
-    <div style={{padding: "8px", marginBottom: "0px"}}>
-      Assign : 
-      <select onChange={e => {setUserId(e.target.value)}} className="select-user border-input">
-        {users.map((v,i) => {
+    <div style={{ padding: "8px", marginBottom: "0px" }}>
+      Assign :
+      <select onChange={e => { setUserId(e.target.value) }} className="select-user border-input">
+        {users.map((v, i) => {
           return <option value={v.id}>{v.name}</option>
         })}
       </select>
     </div>
-    <div>Start Date : <input value={start_date ? new Date(new Date(start_date).toLocaleString("en-US", {timeZone: 'UTC'})).toISOString().substr(0,16) : null} className="border-input" onChange={e => setStartDate(e.target.value)} type="datetime-local"/></div>
-    <div>End Date : <input value={end_date ? new Date(new Date(end_date).toLocaleString("en-US", {timeZone: 'UTC'})).toISOString().substr(0,16) : null} className="border-input" onChange={e => setEndDate(e.target.value)} type="datetime-local"/></div>
+    <div>Start Date : <input value={start_date ? new Date(new Date(start_date).toLocaleString("en-US", { timeZone: 'UTC' })).toISOString().substr(0, 16) : null} className="border-input" onChange={e => setStartDate(e.target.value)} type="datetime-local" /></div>
+    <div>End Date : <input value={end_date ? new Date(new Date(end_date).toLocaleString("en-US", { timeZone: 'UTC' })).toISOString().substr(0, 16) : null} className="border-input" onChange={e => setEndDate(e.target.value)} type="datetime-local" /></div>
     <button className="button-primary" onClick={() => submitShift()}>Add New Shift</button>
   </div>
 }
 
-function ListShift(props){
+function ListShift(props) {
   let history = useHistory()
   const dispatch = useDispatch();
-  const {shift} = useSelector(getShift);
+  const { shift } = useSelector(getShift);
 
   useEffect(() => {
-    dispatch(getAxiosShift({id_user : props.user.id}))
+    dispatch(getAxiosShift({ id_user: props.user.id }))
   }, [dispatch])
 
   let deleteShift = id => {
     // eslint-disable-next-line
-   if(confirm("delete ? ")) {
-    axios.delete('/shift/'+id)
-    .then(res => {
-      NotificationManager.success('delete success', '', 3000);
-      dispatch(getAxiosShift({id_user : props.user.id}))
-    })
-    .catch(err => {
-      NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
-    })
-   }
+    if (confirm("delete ? ")) {
+      axios.delete('/shift/' + id)
+        .then(res => {
+          NotificationManager.success('delete success', '', 3000);
+          dispatch(getAxiosShift({ id_user: props.user.id }))
+        })
+        .catch(err => {
+          NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
+        })
+    }
   }
 
   let publishShift = (id, data) => {
     // eslint-disable-next-line
-   if(confirm("publish ? ")) {
-    axios.put('/shift/'+id, {
-      start_date : data.start_date,
-      end_date : data.end_date,
-      assign_user_id : data.assign_user_id,
-      status : 'published'
-    })
-    .then(res => {
-      NotificationManager.success('publish success', '', 3000);
-      dispatch(getAxiosShift({id_user : props.user.id}))
-    })
-    .catch(err => {
-      NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
-    })
-   }
+    if (confirm("publish ? ")) {
+      axios.put('/shift/' + id, {
+        start_date: data.start_date,
+        end_date: data.end_date,
+        assign_user_id: data.assign_user_id,
+        status: 'published'
+      })
+        .then(res => {
+          NotificationManager.success('publish success', '', 3000);
+          dispatch(getAxiosShift({ id_user: props.user.id }))
+        })
+        .catch(err => {
+          NotificationManager.error(err.response ? err.response.data : 'error : something not right', '', 3000);
+        })
+    }
   }
 
   return <div>
     {Array.isArray(shift.data) && !shift.data.length && "empty data"}
-    {Array.isArray(shift.data) && shift.data.map((v,i) => {
+    {Array.isArray(shift.data) && shift.data.map((v, i) => {
       return <div key={v.start_date} className="shiftCard">
         <h4>{props.user.name} ({v.status})</h4>
-        <div style={{display:"flex", justifyContent: "space-between"}}>
-          <StartEnd start_date={v.start_date} end_date={v.end_date}/>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <StartEnd start_date={v.start_date} end_date={v.end_date} />
           <div>
-            <button onClick={() => history.push("/shift/" +v.id)}>Edit</button>
+            <button onClick={() => history.push("/shift/" + v.id)}>Edit</button>
             <button onClick={() => deleteShift(v.id)}>Delete</button>
             <button onClick={() => publishShift(v.id, v)}>Publish</button>
           </div>
@@ -202,15 +199,15 @@ function ListShift(props){
 }
 
 let StartEnd = (props) => {
-  let {start_date, end_date} = props
+  let { start_date, end_date } = props
   let comp = null
-  if(new Intl.DateTimeFormat('en-GB', { dateStyle: 'short'}).format(new Date(end_date)) === new Intl.DateTimeFormat('en-GB', { dateStyle: 'short'}).format(new Date(start_date))){
+  if (new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(new Date(end_date)) === new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(new Date(start_date))) {
     comp = <p>
       {new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short' }).format(new Date(start_date))} - {new Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(new Date(end_date))}
-      </p>
-  }else {
+    </p>
+  } else {
     comp = <p>
-    {new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short' }).format(new Date(start_date))} - {new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short' }).format(new Date(end_date))}
+      {new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short' }).format(new Date(start_date))} - {new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short' }).format(new Date(end_date))}
     </p>
   }
 
